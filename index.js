8757e88f71977973f1775de9c7fa16de0a7af42e
luthfi1802 <luthfihanif1802@gmail.com>
2024-09-20 13:54:37 +0700

    ubah

--- index.js
+++ index.js
@@ -12,55 +12,12 @@ import Db_databaseRoute from "./Routes/Db_databaseRoute.js";
 import UserRoute from "./Routes/UserRoute.js";
 import AuthRoute from "./Routes/AuthRoute.js";
 import DashboardRoute from "./Routes/DashboardRoute.js"
-// import Produk from "./models/product.js";
-// // import Users from "./models/UserModel.js";
-// import { bionacure } from "./config/database.js";
-// import {bionacure,
-//     dbcmm,
-//     dbgap,
-//     dbputikayo,
-//     dbsinar12,
-//     dbsr12
-// }  from "./config/database.js";
-// import { Organization,
-//     Organization1,
-//     Organization2,
-//     Organization3,
-//     Organization4,
-//     Organization5
-// } from "./models/OrganizationModel.js";
- dotenv.config();
+
+dotenv.config();
 
 
 const app = express();
 
-// try {
-//     await bionacure.authenticate();
-//     console.log('database connected');
-//     await Produk.sync();
-
-//     await dbgap.authenticate();
-//     console.log('database connected');
-//     await Organization1.sync();
-
-//     await dbputikayo.authenticate();
-//     console.log('database connected');
-//     await Organization2.sync();
-
-//     await dbsinar12.authenticate();
-//     console.log('database connected');
-//     await Organization3.sync();
-
-//     await dbsr12.authenticate();
-//     console.log('database connected');
-//     await Organization4.sync();
-
-//     await dbcmm.authenticate();
-//     console.log('database connected');
-//     await Organization5.sync();
-// } catch (error) {
-//     console.log(error)
-// }
 
 app.use(session({
     secret: process.env.SESS_SECRET,
